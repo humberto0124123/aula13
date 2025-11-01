b722f854fe7f1e8158ce819f945571318777aecb
Humberto Correia Moreira Neto <humberto0124123@users.noreply.github.com>
2025-11-01 11:23:01 -0300

    first commit

---
+++ imc.js
@@ -0,0 +1,18 @@
+let peso = Number(prompt("Digite seu peso: "))
+let altura = Number(prompt("Digite sua altura: "))
+
+let imc = peso / (altura * altura) 
+
+if (imc < 18.5) {
+    console.log("Abaixo do peso")
+} else if (imc >= 18.5 && imc < 25) {
+    console.log("Peso normal")
+} else if (imc >= 25 && imc < 30) {
+    console.log("Sobrepeso")
+} else if (imc >= 30 && imc < 35) {
+    console.log("Obesidade grau 1")
+} else if (imc >= 35 && imc < 40) {
+    console.log("Obesidade grau 2")
+} else {
+    console.log("Obesidade grau 3")
+} 
